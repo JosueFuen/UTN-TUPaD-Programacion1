15bd9d528d6e0e9d706b4d922587f14ca302740a
JosueFuen <fosuedavid@yahoo.com.ar>
2025-09-25 18:46:03 +0000

    acualizacion

--- Unidad 05 - Listas/TP05.py
+++ Unidad 05 - Listas/TP05.py
@@ -12,8 +12,67 @@ suma= sum(lista)
 promedio= suma/cantidad
 menor_nota=min(lista)
 mayor_nota=max(lista)
-
-print (f"""La lsita de notas es {lista}
-La nota mas alta de la lista es {mayor_nota} 
+for nota in lista:
+    print (nota)
+print (f"""La nota mas alta de la lista es {mayor_nota} 
 La nota mas baja de la lista es {menor_nota}
 El promedio de notas es {promedio}""")
+
+#2)Pedir al usuario que cargue 5 productos en una lista.
+#•Mostrar la lista ordenada alfabéticamente. Investigue el uso del método sorted().
+#•Preguntar al usuario qué producto desea eliminar y actualizar la lista.
+lista=[]
+opcion= "e"
+for x in range (5):
+    producto = input("Ingrese un producto: ")
+    lista.append(producto)
+
+print("\n¿Desea eliminar o agregar u producto?")
+while opcion !="X":
+    opcion=input("Presione A para agregar, E para eliminar, X para salir:")
+    opcion=opcion.upper()
+    if opcion == "A":
+        producto=input("Inserte el producto que desea agregar: ")
+        lista.append(producto)
+    elif opcion=="E":
+        producto=input("Inserte el producto que desea eliminar: ")
+        if producto in lista:
+            lista.remove(producto)
+        else:
+            print("El producto no está en la lista")
+    elif opcion!="X":
+        print("Opción incorrecta. Vuelva a ingresar una opción:")
+lista1=sorted(lista)
+print("\nLista de productos ordenada: ")
+for productos in lista1:
+    print(f"-{productos}")
+
+#3)Generar una lista con 15 números enteros al azar entre 1 y 100.
+#•Crear una lista con los pares y otra con los impares.
+#•Mostrar cuántos números tiene cada lista.
+import random
+lista=[]
+par=[]
+impar=[]
+npar=0
+nimpar=0
+for x in range (15):
+    num=random.randint(1,100)
+    lista.append(num)
+    if num%2==0:
+        par.append(num)
+        npar+=1
+    else:
+        impar.append(num)
+        nimpar+=1
+print("Lista de numeros: ") 
+for y in lista:
+    print(y)
+print ("\nLista de pares: ")
+for z in par:
+    print(z)
+print(f"Total de pares: {npar}")
+print("\nLista de impares: ")
+for s in impar:
+    print(s)
+print(f"Total de impares: {nimpar}")
---
+++ Unidad 05 - Listas/pruebas.py
@@ -0,0 +1,9 @@
+#4)Dada una lista con valores repetidos:[1,3,5,3,7,1,9,5,3]
+#•Crear una nueva lista sin elementos repetidos.
+#•Mostrar el resultado.
+import random
+lista=[1, 3, 5, 3, 7, 1, 9, 5, 3]
+lista1=lista
+nuevalista=[]
+for num in lista:
+   
