6a7c1aee646d500e6ad465760d8adb2064c48467
JosueFuen <fosuedavid@yahoo.com.ar>
2025-09-26 00:09:57 -0300

    Update pruebas.py
    hasta el ejercicio 6

--- Unidad 05 - Listas/pruebas.py
+++ Unidad 05 - Listas/pruebas.py
@@ -1,9 +1,5 @@
-#4)Dada una lista con valores repetidos:[1,3,5,3,7,1,9,5,3]
-#•Crear una nueva lista sin elementos repetidos.
-#•Mostrar el resultado.
-import random
-lista=[1, 3, 5, 3, 7, 1, 9, 5, 3]
-lista1=lista
-nuevalista=[]
-for num in lista:
-   
+#3)Crear una matriz (lista anidada) de 7x2 con las temperaturas mínimas y máximas de una semana. 
+#• Calcular el promedio de las mínimas y el de las máximas. 
+#• Mostrar en qué día se registró la mayor amplitud térmica. 
+for i in range(7):
+    
