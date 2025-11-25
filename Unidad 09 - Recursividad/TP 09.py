4a94f5609ac1df6c9627e3f73bad21b0dcc3a96e
JosueFuen <fosuedavid@yahoo.com.ar>
2025-11-25 17:29:06 -0300

    Finalizado

--- Unidad 09 - Recursividad/TP 09.py
+++ Unidad 09 - Recursividad/TP 09.py
@@ -73,12 +73,12 @@ else:
 #6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
 #número entero positivo y devuelva la suma de todos sus dígitos.
 def suma_digitos(n):
-    if len(n)<2:
-        return int(n)
+    if n<10:
+        return n
     else:
-        return suma_digitos(n[:-1]) + int(n[-1])
-num=input('Ingrese un numero: ')
-print (suma_digitos(num))
+        return (n % 10) + suma_digitos(n // 10)
+num=int(input('Ingrese un numero: '))
+print (f'La suma es {suma_digitos(num)}')
 
 #7)Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
 #bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
